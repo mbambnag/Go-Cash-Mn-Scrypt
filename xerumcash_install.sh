f22928e03aa3f991e9aeeac1b848957c5a5c137b
Bit Yoda <36884085+Realbityoda@users.noreply.github.com>
2018-04-18 13:29:49 -0700

    Update xerumcash_install.sh
    Corrected Port Number.

--- xerumcash_install.sh
+++ xerumcash_install.sh
@@ -9,7 +9,7 @@ COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/Realbityoda/Xerumcash/releases/download/Xerumcash/Xerumcash.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='XerumCash'
-COIN_PORT=4330
+COIN_PORT=32081
 
 
 NODEIP=$(curl -s4 icanhazip.com)
